aad8dbe587a5bc6e88157e5824390b1462f7eb9d
pratik-karna <kaystha.pratik@gmail.com>
2023-02-11 16:51:30 +0530

    2nd

--- name.py
+++ name.py
@@ -1,11 +1,12 @@
-name = "P"  
-Name = "R"  
-naMe = "A"  
-NAME = "T"  
-n_a_m_e = "I"  
-_name = "K"  
-name_ = "D"  
-_name_ = "O"  
-na56me = "N"  
-  
-print(name,Name,naMe,NAME,n_a_m_e, NAME, n_a_m_e, _name, name_,_name, na56me)  
+name = "P"
+Name = "R"
+naMe = "A"
+NAME = "T"
+n_a_m_e = "I"
+_name = "K"
+name_ = "D"
+_name_ = "O"
+na56me = "N"
+
+print(name, Name, naMe, NAME, n_a_m_e, NAME,
+      n_a_m_e, _name, name_, _name, na56me)
