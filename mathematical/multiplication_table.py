cfa4b4ee17b82ea1e95f647679affac0c2c0c124
pratik-karna <kaystha.pratik@gmail.com>
2023-04-11 00:05:35 +0530

    first commit

---
+++ mathematical/multiplication_table.py
@@ -0,0 +1,8 @@
+print("Enter a Number: ")
+num = int(input())
+k = 1
+print("\nMultiplication Table:")
+for i in range(10):
+  mul = num*k
+  print(mul)
+  k = k+1
---
+++ pattern/Star, Pyramid, Number Patterns/inverted_half_pyramid.py
@@ -0,0 +1,5 @@
+print("Inverted Half Pyramid of Stars (*):")
+for i in range(5):
+    for j in range(i, 5):
+        print("* ", end="")
+    print()
