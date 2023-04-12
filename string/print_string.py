dbb4a40f34aac1a7395cc06d1b38abc7d98330c7
pratik-karna <kaystha.pratik@gmail.com>
2023-04-12 16:44:50 +0530

    first commit

---
+++ string/print_string.py
@@ -0,0 +1,12 @@
+print("How many string to enter ? ", end="")
+noOfString = int(input())
+
+print("Enter", noOfString, "Strings followed by ENTER key: ", end="")
+mylist = list()
+for i in range(noOfString):
+    mystring = input()
+    mylist.append(mystring)
+
+print("\nStrings entered by you are:")
+for i in range(noOfString):
+    print("String No.", i+1, ": \"", mylist[i], "\"", sep="")
